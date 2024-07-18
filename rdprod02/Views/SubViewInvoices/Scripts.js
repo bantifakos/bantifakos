49257924ce87859895b8b6bfab7f1149cb73e038
Vasilis Antifakos <ban@es.entersoft.gr>
2024-07-18 13:01:25 +0300

    Removed rdprod02

--- rdprod02/Views/SubViewInvoices/Scripts.js
+++
@@ -1,4 +0,0 @@
-(function(es, { EQUALS, ISNULL, EQUALS_CI, IS_IN, IS_IN_CI }, {  }) {
-es.Action.fn_SubViewInvoices_0 = function($args) { return $args.inactive || ISNULL($args.inactive);};
-
-})(window.es, window.es.AppHelpers || { }, window.es.Action || { });
--- rdprod02/Views/document/Scripts.js
+++
@@ -1,8 +0,0 @@
-(function(es, { EQUALS, ISNULL, EQUALS_CI, IS_IN, IS_IN_CI }, {  }) {
-es.Action.fn_document_0 = function($args) {
- if ($args.lineType == 6) return 0;
-else if ($args.lineType == 5) return 1;
-else return;
-};
-
-})(window.es, window.es.AppHelpers || { }, window.es.Action || { });
